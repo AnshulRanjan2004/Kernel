d030ac4173cf53b359e8db6d08f9bc41bddef202
Anshul Ranjan <ItsAnshulRanjan@gmail.com>
2023-04-23 20:42:29 +0530

    kernel version 2

--- kernel.h
+++ kernel.h
@@ -7,6 +7,14 @@
 
 typedef unsigned short UINT16;
 
+
+int DIGIT_ASCII_CODES[10] = {0x30, 0x31, 0x32, 0x33, 0x34, 0x35, 0x36, 0x37, 0x38, 0x39};
+
+
+unsigned int VGA_INDEX;
+
+#define BUFSIZE 2200
+
 UINT16* TERMINAL_BUFFER;
 
 #endif
